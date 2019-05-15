66258050f1f91e119d88fcb039be964a927af64b
真下悠之 <hisayuki-mashimo@gyoumuyouPC.local>
2019-05-16 00:16:36 +0900

    方向転換処理調整

--- Buta/Assets/Transform.cs
+++ Buta/Assets/Transform.cs
@@ -4,40 +4,48 @@ using UnityEngine;
 
 public class Transform : MonoBehaviour
 {
+    GameObject MainCamera;
+
     public float speed = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.MainCamera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
         Animator Animtor = GetComponent<Animator>();
+
         bool Act = false;
-        /*
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Animtor.SetBool("Act", (Act == true) ? false : true);
+
+        if (Input.GetKey("left")) {
+            this.transform.position += this.transform.forward * speed * Time.deltaTime;
+            this.MainCamera.transform.position += this.transform.forward * speed * Time.deltaTime;
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+            Act = true;
         }
-        */
         if (Input.GetKey("up")) {
-            transform.position += transform.forward * speed * Time.deltaTime;
+            this.transform.position += this.transform.forward * speed * Time.deltaTime;
+            this.MainCamera.transform.position += this.transform.forward * speed * Time.deltaTime;
+            this.transform.rotation = Quaternion.Euler(0.0f, 90, 0.0f);
             Act = true;
         }
         if (Input.GetKey("right")) {
-            transform.position += transform.right * speed * Time.deltaTime;
+            this.transform.position += this.transform.forward * speed * Time.deltaTime;
+            this.MainCamera.transform.position += this.transform.forward * speed * Time.deltaTime;
+            this.transform.rotation = Quaternion.Euler(0.0f, 180, 0.0f);
             Act = true;
         }
         if (Input.GetKey("down")) {
-            transform.position -= transform.forward * speed * Time.deltaTime;
+            this.transform.position += this.transform.forward * speed * Time.deltaTime;
+            this.MainCamera.transform.position += this.transform.forward * speed * Time.deltaTime;
+            this.transform.rotation = Quaternion.Euler(0.0f, 270, 0.0f);
             Act = true;
         }
-        if (Input.GetKey("left")) {
-            transform.position -= transform.right * speed * Time.deltaTime;
-            Act = true;
-        }
+
         Animtor.SetBool("Act", Act);
     }
 }
